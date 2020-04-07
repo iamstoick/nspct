88d458b26b5b57fe4411d8228c81c4f6ba20ec5f
Gerald Villorente <geraldvillorente@gmail.com>
2020-04-07 19:53:04 +0800

    DNS records

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"math/rand"
 	"time"
 	"os"
+	"net"
 	c "github.com/fatih/color"
 	//spew "github.com/davecgh/go-spew/spew"
 )
@@ -130,6 +131,78 @@ func getURLHeader (url string) string {
 	return ""
 }
 
+/*
+Get DNS information.
+*/
+func dnsQuery (url string) {
+    p := c.New(c.FgYellow, c.Bold)
+	fmt.Println()
+	
+	// Query IP address.
+	p.Printf("Querying IP records...\n")
+	iprecords, ipeErr := net.LookupIP(url)
+	if ipeErr != nil {
+		panic(ipeErr)
+	}
+	for _, ip := range iprecords {
+		fmt.Println(ip)
+	}
+   
+	fmt.Println()
+
+	// Query CNAME records.
+	p.Printf("Querying CNAME records...\n")
+	cname, cnameErr := net.LookupCNAME(url)
+	if cnameErr != nil {
+		panic(cnameErr)
+	}
+	fmt.Printf("%s\n", cname)
+
+	fmt.Println()
+
+	// Query MX records.
+	p.Printf("Querying MX records...\n")
+	mxs, mxErr := net.LookupMX(url)
+	if mxErr != nil {
+		panic(mxErr)
+	}
+	for _, mx := range mxs {
+		fmt.Printf("%s %v\n", mx.Host, mx.Pref)
+	}
+
+	fmt.Println()
+
+	// Query NS records.
+	p.Printf("Querying NS records...\n")
+	nss, nsErr := net.LookupNS(url)
+	if nsErr != nil {
+		panic(nsErr)
+	}
+	if len(nss) == 0 {
+		fmt.Printf("no record")
+	}
+	for _, ns := range nss {
+		fmt.Printf("%s\n", ns.Host)
+	}
+
+	fmt.Println()
+
+	// Query txt records.
+	p.Printf("Querying TXT records...\n")
+	txts, txtErr := net.LookupTXT(url)
+	if txtErr != nil {
+		panic(txtErr)
+	}
+	if len(txts) == 0 {
+		fmt.Printf("no record")
+	}
+	for _, txt := range txts {
+		fmt.Printf("%s\n", txt)
+	}
+
+	fmt.Println()
+}
+
 func main () {
 
 	rand.Seed(time.Now().UnixNano())
@@ -162,11 +235,15 @@ func main () {
  
 	if !*cache {
 		queryParam := randSeq(10)
-		url := fmt.Sprintf("%s?query-param=%s", *urlRaw, queryParam)
+		url := fmt.Sprintf("http://%s?query-param=%s", *urlRaw, queryParam)
 		getURLHeader(url)
 	} else {
-		getURLHeader(*urlRaw)
+		url := fmt.Sprintf("http://%s", *urlRaw)
+		getURLHeader(url)
 	}
+
+	// Check DNS records.
+	dnsQuery(*urlRaw)
 	
     /*
 	spew.Dump(scope)
